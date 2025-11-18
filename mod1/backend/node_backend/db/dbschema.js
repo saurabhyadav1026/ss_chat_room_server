a608948e864fd46b21e57c8a263483364cecb90f
Saurabh Yadav <SAURABHYADAV7041916@GMAIL.COM>
2025-11-18 17:57:38 +0530

    Refactor user schema to include specific fields

--- mod1/backend/node_backend/db/dbschema.js
+++ mod1/backend/node_backend/db/dbschema.js
@@ -1,47 +1,55 @@
 
 
 
-const user_schema=mongoose.Schema({
+const user_schema=new mongoose.Schema({
                                   
-                                  socketId:[String];
-                                  public_info:Object,
-                                  private_info:Object,
+                                  socketId:[String],
+                                  public_info:{
+                                    username:{type:String,require:true,unique:true}
+                                  },
+                                  private_info:{
+                                    mailId:{type:String,required:true,unique:true}
+                                  },
 })
 
-const User=mongosse.model('User',user_schema);
+const User=mongoose.model('User',user_schema);
 export User;
 
 
 
-const chat_room_schema=mongoose.Schema({
-  members:[ {type:mongoose.Schema.Type.ObjectId, ref:User  ,required:true}]
+const chat_room_schema=new mongoose.Schema({
+  members:[{  memberId:{type:mongoose.Schema.Types.ObjectId, ref:"User"  ,required:true}, unreadCount: Number}],
+  
 last_msg:String,
 room_setting:Object
 })
 
-const Chat_Room =mongoose.model('Chat_Room',chat_rooom_schema);
-export Chat_room;
+const Chat_Room =mongoose.model('Chat_Room',chat_room_schema);
+export Chat_Room;
 
 
 
 
 
 
-const message_schema=mongoose.model({
+const message_schema=new mongoose.Schema({
 
-  roomId:{ type:mongoose.Schema.Types.ObjectId, ref: Chat_room ,required:true},
-  senderId:{type:mongoose.Schema.Types.ObjectId, ref:User,required:true},
+  roomId:{ type:mongoose.Schema.Types.ObjectId, ref:"Chat_Room" ,required:true},
+  senderId:{type:mongoose.Schema.Types.ObjectId, ref:"User",required:true},
   text:String,
   timeStamp:{type:Date,default:Date.now},
   tick:{type:Number ,required:true},
-  tickStatus:[Object]
-  deleteFor:[{type:mongoose.Schema.Types.ObjectId, ref:User}],
+  tickStatus:[Object],
+  deleteFor:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}],
   beforeEdit:[{text:String,timeStamp:{type:Date,default:Date.now}}],
   attachmentFile:[String]
   
 })
 
-export const Message=mongoose.model('Message',message_schema)
+ const Message=mongoose.model('Message',message_schema);
+
+export Message;
+
 
 
 
