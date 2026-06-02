7dab7e5548644c37798a100dbcc81c0b4817b452
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-06-02 13:15:12 +0530

    cor fix remove async

--- mod1/backend/node_backend/socketcomuniation/config/mainsocket.js
+++ mod1/backend/node_backend/socketcomuniation/config/mainsocket.js
@@ -5,11 +5,11 @@ import connectUser from "./socket-connection/connectUser.js";
 import connectCall from "./socket-connection/connectCall.js";
 
 
- export const socketIntegration=async(server)=>{
+ export const socketIntegration=(server)=>{
     console.log("we are here 4321")
     const io = new Server(server, {
   cors: {
-    origin:"*" , // process.env.FRONTEND_BASEURL,
+    origin: process.env.FRONTEND_BASEURL,
     methods: ["GET", "POST"],
    // credentials: true
   }
