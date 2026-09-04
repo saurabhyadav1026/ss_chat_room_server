7f975d527fadb1a1458a7d776e47a0115cbb7013
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-09-05 03:11:32 +0530

    cloudinary storage fix

--- mod1/backend/node_backend/multer/config.js
+++ mod1/backend/node_backend/multer/config.js
@@ -1,22 +1,15 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import cloudinary from './cloudinary.js';
-
+import streamifier from "streamifier";
 
 
 //cloudinary storage configuration
 
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'uploads', // Specify the folder in Cloudinary where files will be stored
-    allowed_formats: ['jpg', 'jpeg', 'png', 'gif'], // Specify allowed file formats
-  },
-});
 
 
-const upload = multer({ storage: storage });
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 
 
@@ -30,6 +23,7 @@ export const uploadeDPByImageUrl=async (userId,url)=>{
     folder: "users_dp",
     public_id: `user_${userId}`,
        overwrite: true,
+          allowed_formats: ['jpg', 'jpeg', 'png', 'gif'], // Specify allowed file formats
   });
   return result.secure_url;
 }
@@ -38,3 +32,65 @@ catch(err){
   return null;
 }
 }
+
+export const uploadeDPByImageFile=async(userId,file)=>{
+  
+  try{
+     const result = await new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        { 
+ folder: "users_dp",
+    public_id: `user_${userId}`,
+       overwrite: true,
+   allowed_formats: ['jpg', 'jpeg', 'png', 'gif'], // Specify allowed file formats
+         },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        }
+      );
+
+      streamifier.createReadStream(file.buffer).pipe(stream);
+    });
+
+  return result.secure_url;
+}
+catch(err){
+  console.error(err);
+  return null;
+}
+}
+
+
+
+
+/**
+ * @swagger
+ * /uploads:
+ *   post:
+ *     summary: Upload a file
+ *     tags:
+ *       - Uploads
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - file
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: File uploaded successfully
+ */
+/* 
+  app.post('/uploads',upload.single('file'), async (req, res) => {
+    // Handle the uploaded file here
+  
+    res.status(200).json({ message: 'File uploaded successfully' });
+  });
+ */
